24df1fc00be3a42479fec60d365973bf0767732f
zyuzhi <zyuzhi78@gmail.com>
2018-11-30 11:37:59 +0800

    function class for functional module

--- functional/function.h
+++ functional/function.h
@@ -18,10 +18,34 @@ struct function_traits<R (C::*)(Args...) const> : function_traits<R (C::*)(Args.
 
 template<typename R, typename ...Args>
 struct function_traits<R (Args...)> {
-  using RawType = R(Args...);
+  using RawType = R(*)(Args...);
   using Return = R;
 };
 
 template<typename R, typename ...Args>
 struct function_traits<R (Args...) const> : function_traits<R (Args...)> {};
+
+template<typename F>
+struct function {
+  using Function = typename function_traits<F>::RawType;
+  using Return = typename function_traits<F>::Return;
+  function() = delete;
+  explicit function(const Function& f) : f_(f) {}
+  explicit function(Function&& f) : f_(std::forward<Function>(f)) {}
+  function(const function &func) : f_(func.f_) {}
+  function(function &&func) : f_(std::move(func.f_)) {}
+
+  template<typename ...Args>
+  Return operator()(Args&&... args) {
+    return f_(std::forward<Args>(args)...);
+  }
+
+  template<typename Class, typename ...Args>
+  Return operator()(Class* c, Args&&... args) {
+    return (c->*f_)(std::forward<Args>(args)...);
+  }
+ private:
+  Function f_;
+};
+
 }
--- main.cpp
+++ main.cpp
@@ -12,7 +12,24 @@ void test_tuple() {
 }
 
 void test_functional() {
+  struct Foo {
+    Foo(int i) : i_(i) {}
+    void echo() {
+      std::cout << i_ << std::endl;
+    }
+   private:
+    int i_;
+  };
+  using namespace functional;
   std::cout << std::is_same<typename functional::function_traits<decltype(test_tuple)>::Return, void>::value << std::endl;
+  const auto lambda_f = [](){return 1;};
+  function<int()> f1(lambda_f);
+  function<void()> f2(test_tuple);
+  function<void (Foo::*)()> f3(&Foo::echo);
+  std::cout << f1() << std::endl;
+  f2();
+  Foo o(4);
+  f3(&o);
 }
 
 int main() {
